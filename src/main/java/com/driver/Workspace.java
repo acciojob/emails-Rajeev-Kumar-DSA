86b17bf113703fd50ebdda5615c6e94a1b6d56b4
Rajeev-Kumar-DSA <rajeevkr2061998@gmail.com>
2024-06-03 21:54:02 +0530

    update the workspace count

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -6,6 +6,7 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Workspace extends Gmail{
 
@@ -28,33 +29,50 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-        int attendMeetingCount = 0;
+
+        if(calendar.isEmpty()){
+            return 0;
+        }
 
         // here we can attend any one in schedule time
         // its releated to job scheduling problem
         // first I have to sort the start time or endtime. then I will check how much meeting without overlapping
 
-        ArrayList<Pair<LocalTime, Integer>> endTimes = new ArrayList<>();
-        for (int i=0; i<calendar.size(); i++){
-            endTimes.add(Pair.of(calendar.get(i).getEndTime(), i));
-        }
+//        ArrayList<Pair<LocalTime, Integer>> endTimes = new ArrayList<>();
+//        for (int i=0; i<calendar.size(); i++){
+//            endTimes.add(Pair.of(calendar.get(i).getEndTime(), i));
+//        }
+//
+//        Collections.sort(endTimes);
+//
+//        LocalTime limitOfTime = endTimes.get(0).getLeft();
+//
+//        if(!endTimes.isEmpty()){
+//            attendMeetingCount ++;
+//        }
+//
+//        for (int i=1; i<endTimes.size(); i++){
+//            if(calendar.get(endTimes.get(i).getRight()).getStartTime().compareTo(limitOfTime) >= 0){
+//                attendMeetingCount ++;
+//                limitOfTime = endTimes.get(i).getLeft();
+//            }
+//        }
+//
+//
+//        return attendMeetingCount;
 
-        Collections.sort(endTimes);
+        Collections.sort(calendar, Comparator.comparing(Meeting :: getEndTime));
+        int attendMaximumMeetingCount = 1;
 
-        LocalTime limitOfTime = endTimes.get(0).getLeft();
-
-        if(!endTimes.isEmpty()){
-            attendMeetingCount ++;
-        }
-
-        for (int i=1; i<endTimes.size(); i++){
-            if(calendar.get(endTimes.get(i).getRight()).getStartTime().compareTo(limitOfTime) >= 0){
-                attendMeetingCount ++;
-                limitOfTime = endTimes.get(i).getLeft();
+        LocalTime lastEndTime = calendar.get(0).getEndTime();
+        for(int i=1; i<calendar.size(); i++){
+            if(calendar.get(i).getStartTime().isAfter(lastEndTime)){
+                attendMaximumMeetingCount ++;
+                lastEndTime = calendar.get(i).getEndTime();
             }
         }
 
+        return attendMaximumMeetingCount;
 
-        return attendMeetingCount;
     }
 }
